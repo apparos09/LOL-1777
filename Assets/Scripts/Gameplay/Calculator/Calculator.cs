0bf5153ff0a775fe0e05a0350a3fb56e9ffdae7a
mecha-rm <roderick.montague243@gmail.com>
2025-01-09 00:39:18 -0500

    Updated Calculator (WIP)
    Updated the calculator with a script to solve equations. It doesn't work properly, but the set up is there. The calculator is going to need a back button and a smaller clear button. The tutorials also still need to be updated to reflect the current game.

--- Assets/Scripts/Gameplay/Calculator/Calculator.cs
+++ Assets/Scripts/Gameplay/Calculator/Calculator.cs
@@ -11,11 +11,23 @@ namespace RM_MST
         // The text for the calculator display.
         public TMP_Text displayText;
 
+        // The maximum number of characters for the calculator display.
+        public const int DISPLAY_CHAR_MAX = 40;
+
+        // If 'true', the character limit is used.
+        private bool useCharLimit = true;
+
         // The keys for the calculator.
         public List<CalculatorKey> keys = new List<CalculatorKey>();
 
         // Used to see if the calculator is interactable.
         private bool interactable = true;
+
+        // The error string.
+        private string errorStr = "Error";
+
+        // The error string key.
+        public const string ERROR_STR_KEY = "kwd_error";
 
         // Start is called before the first frame update
         void Start()
@@ -26,6 +38,14 @@ namespace RM_MST
                 keys = new List<CalculatorKey>(GetComponentsInChildren<CalculatorKey>());
             }
 
+            // If the LOL SDK and the LOL Manager is initialized.
+            if(LOLManager.IsInstantiatedAndIsLOLSDKInitialized())
+            {
+                // Translates the error string.
+                errorStr = LOLManager.Instance.GetLanguageText(ERROR_STR_KEY);
+            }
+
+
             // Clear the calculator to start.
             Clear();
         }
@@ -33,9 +53,20 @@ namespace RM_MST
         // Adds the key's character.
         public void AddCharacterToEquation(char keyChar)
         {
+            // If the display text is the error string, clear it.
+            if(displayText.text == errorStr)
+            {
+                Clear();
+            }
+
             // Checks what kind of character has been sent.
             switch(keyChar)
             {
+                case 'B':
+                case 'b': // "B" means Backspace
+                    RemoveLastCharacter();
+                    break;
+
                 case 'C': // "C" means Clear
                 case 'c':
                     Clear();
@@ -46,7 +77,14 @@ namespace RM_MST
                     break;
 
                 default:
-                    displayText.text += keyChar;
+                    // If the display text plus 1 is below the max char count...
+                    // Add the character. This only happens if the character limit is enabled.
+                    // If the character limit is disabled, just add characters no matter what.
+                    if((useCharLimit && displayText.text.Length + 1 <= DISPLAY_CHAR_MAX) || !useCharLimit)
+                    {
+                        displayText.text += keyChar;
+                    }
+                    
                     break;
             }
         }
@@ -55,8 +93,47 @@ namespace RM_MST
         // The result is saved in the display text.
         public bool TrySolve()
         {
-            Debug.Log("TODO: Implement Solve!");
-            return false;
+            // Sets the equation.
+            string equation = displayText.text;
+
+            // Replaces all "X" and "x" with "*".
+            equation = equation.Replace("X", "*");
+            equation = equation.Replace("x", "*");
+
+            // Calculates the result.
+            string result = util.CustomMath.CalculateMathString(equation);
+
+            // Checks if the result is valid, and returns the result.
+            bool valid = result != "";
+
+            // Sets the display text's result.
+            // A blank string is returned when a calculation fails.
+
+            // If the string is valid, set it as the display string.
+            if(valid)
+            {
+                displayText.text = result;
+            }
+            else // Not valid, so set the display text as an error.
+            {
+                displayText.text = errorStr;
+            }
+
+            // Returns valid.
+            return valid;
+        }
+
+        // Removes the last character in the string.
+        public void RemoveLastCharacter()
+        {
+            // There are characters to remove.
+            if(displayText.text.Length > 0)
+            {
+                // Gets the string, removes the last character, and sets it.
+                string str = displayText.text;
+                str = str.Remove(str.Length - 1);
+                displayText.text = str;
+            }
         }
 
         // Clears the equation.
